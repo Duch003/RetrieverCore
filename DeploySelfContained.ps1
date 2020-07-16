d66adf0123f95f3d3da8c38fffb248d724c4b851
Duch003 <tomaszmankin003@gmail.com>
2020-07-16 23:19:31 +0200

    Deployment process trot has been added.

---
+++ DeploySelfContained.ps1
@@ -0,0 +1,5 @@
+cd ..
+dotnet publish -p TestConsole --self-contained true -r win10
+
+#https://docs.microsoft.com/en-us/dotnet/core/rid-catalog RIDs
+#https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-publish Deployment process
